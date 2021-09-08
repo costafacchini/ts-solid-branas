e9a80a5c825b666bd39bc14e2a911a94cb5895ae
Alan Costa Facchini <costafacchini@gmail.com>
2021-09-08 12:41:53 -0300

    Adicionar a capacidade de calcular os impostos do pedido

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -12,3 +12,13 @@ test('Deve criar um pedido', () => {
   expect(total).toBe(111)
 })
 
+test('Deve calcular os impostos', () => {
+  const order = new Order()
+  order.AddItem(new Item('Beer', 'Budweiser', 10)) //10:
+  order.AddItem(new Item('Whisky', 'Jack Daniels', 100)) //20
+  order.AddItem(new Item('Water', 'Crystal', 1)) //Isento
+
+  const impostos = order.getTaxes()
+
+  expect(impostos).toBe(21)
+})
--- src/Order.ts
+++ src/Order.ts
@@ -11,6 +11,22 @@ export default class Order {
     this.items.push(item)
   }
 
+  getTaxes() {
+    let taxes = 0
+
+    for (const item of this.items) {
+      if (item.category === 'Beer') {
+        taxes += item.price * 0.1
+      }
+
+      if (item.category === 'Whisky') {
+        taxes += item.price * 0.2
+      }
+    }
+
+    return taxes
+  }
+
   getTotal() {
     return this.items.reduce((prev, curr) => prev + curr.price, 0)
   }
